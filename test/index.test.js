38582353777eb7266df81c9dd3ac4f228b3ec46c
Francesco Sullo <sullof@gmail.com>
2021-02-08 14:28:54 -0800

    [publish binary]

--- test/index.test.js
+++ test/index.test.js
@@ -11,29 +11,22 @@ describe('#RustLib', function () {
   it('should encrypt and decrypt a string', async function () {
 
     let encrypted = RustLib.encrypt(plaintext, key)
-
     let decrypted = RustLib.decrypt(encrypted, key)
-
     assert.equal(plaintext, decrypted)
   })
 
   it('should encrypt in JS and decrypt in Rust', async function () {
 
     let encrypted = Crypto.encrypt(plaintext, key)
-
     let decrypted = RustLib.decrypt(encrypted, key)
-
     assert.equal(plaintext, decrypted)
   })
 
   it('should encrypt in Rust and decrypt in JS', async function () {
 
     let encrypted = RustLib.encrypt(plaintext, key)
-
     let decrypted = Crypto.decrypt(encrypted, key)
-
     assert.equal(plaintext, decrypted)
-
   })
 
 })
